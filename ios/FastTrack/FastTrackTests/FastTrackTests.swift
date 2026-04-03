c0824bde24dc482ade583b498d99d94d8ea4f5f8
Jameson Toper <jtoper@users.noreply.github.com>
2026-04-02 20:45:12 -0700

    testing framework and release tracking

---
+++ ios/FastTrack/FastTrackTests/FastTrackTests.swift
@@ -0,0 +1,18 @@
+//
+//  FastTrackTests.swift
+//  FastTrackTests
+//
+//  Created by Jameson Toper on 4/2/26.
+//
+
+import Testing
+
+struct FastTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
