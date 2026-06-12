16af050e9c86343f449209c41ae951a497be79f9
Jameson Toper <jtoper@users.noreply.github.com>
2026-06-11 17:46:41 -0700

    chore(ios): guard recordingStartTime debug print (E-14)

--- ios/FastTrack/FastTrack/ViewModels/DriveManager.swift
+++ ios/FastTrack/FastTrack/ViewModels/DriveManager.swift
@@ -170,7 +170,9 @@ class DriveManager: ObservableObject {
         attempts060 = []
 
         #if DEBUG
-        print("⏰ Recording start time: \(recordingStartTime!)")
+        if let start = recordingStartTime {
+            print("⏰ Recording start time: \(start)")
+        }
         #endif
 
         // Reset extended stats
